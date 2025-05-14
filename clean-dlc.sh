410774fcf47b3e35e45e3c01801a98bc02207af7
Bronco Oostermeyer <dev@bfv.io>
2025-05-14 21:28:03 +0200

    remove critical CVE (sonic), added clean-dlc.sh

---
+++ clean-dlc.sh
@@ -0,0 +1,20 @@
+rm -rf /usr/dlc/*OpenEdge*
+rm -f  /usr/dlc/ade.pf
+rm -rf /usr/dlc/ant/manual
+rm -f  /usr/dlc/empty*
+rm -rf /usr/dlc/gradle
+rm -f  /usr/dlc/inst_hlp
+#rm -rf /usr/dlc/install*  ## DO NOT DELETE FOR 12.8
+rm -f /usr/dlc/install/esam/*
+
+rm -rf /usr/dlc/odbc
+#rm -rf /usr/dlc/perl
+rm -rf /usr/dlc/prohelp
+rm -rf /usr/dlc/servers/redist
+rm -rf /usr/dlc/sonic
+rm -rf /usr/dlc/src
+rm -rf /usr/dlc/templates
+rm -rf /usr/dlc/toolkit
+rm -rf /usr/dlc/tty/adm
+rm -rf /usr/dlc/tty/adm2
+rm -rf /usr/dlc/ubqmanager
